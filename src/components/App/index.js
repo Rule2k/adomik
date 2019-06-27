6c230788888edcb6ddd9da461ac27eedcdd95559
Rule2k <nicolaspaviot@gmail.com>
2019-06-27 09:54:27 +0200

    adding server to app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,10 @@
-/**
- * Npm import
- */
 import React from 'react';
 
-/**
- * Local import
- */
 import Example from 'src/containers/Example';
 import './app.scss';
 
-/**
- * Code
- */
 const App = () => (
   <Example />
 );
 
-/**
- * Export
- */
 export default App;
