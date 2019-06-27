9faeda83e73af19f8046929b251cec13e48d2a44
Rule2k <nicolaspaviot@gmail.com>
2019-06-27 12:03:49 +0200

    changing dropdown dumb component into a class to be able to call componentDidMount method : this will call the action dropdownvaluehaschanged when the component is render to be able to automatically put the value of the selected value into redux's state. (before that change, you had to change at least once the value to put it into redux)

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import PropsTypes from 'prop-types';
 
-const Dropdown = ({ dropdownValueHasChanged, dropdownValue }) => {
-  const handleNewValue = event => (
-    dropdownValueHasChanged(event.target.value)
-  );
-  return (
-    <select id="dropdown" onChange={handleNewValue} value={dropdownValue}>
-      <option value="Choisissez une ville">Choisissez une ville</option>
-      <option value="Paris">Paris</option>
-      <option value="Marseille">Marseille</option>
-    </select>
-  );
-};
+
+class Dropdown extends React.Component {
+  componentDidMount() {
+    const { dropdownValueHasChanged } = this.props;
+    dropdownValueHasChanged(document.getElementById('dropdown').value);
+  }
+
+  render() {
+    const { dropdownValueHasChanged, dropdownValue } = this.props;
+    const handleNewValue = event => (
+      dropdownValueHasChanged(event.target.value)
+    );
+    return (
+      <select id="dropdown" onChange={handleNewValue} value={dropdownValue}>
+        <option value="Choisissez une ville" disabled>Choisissez une ville</option>
+        <option value="Paris">Paris</option>
+        <option value="Marseille">Marseille</option>
+      </select>
+    );
+  }
+}
+
 
 export default Dropdown;
